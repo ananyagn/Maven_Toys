25cb7376dad4d67281b26cf40b1760afcb055816
gnananya2@gmail.com <gnananya2@gmail.com>
2025-07-21 10:01:50 +0000

    bronze updated

---
+++ models/bronze/brn_inv.sql
@@ -0,0 +1,9 @@
+
+ 
+    select
+        cast("STORE_ID" as varchar) as store_id,
+        cast("PRODUCT_ID" as varchar) as product_id,
+        try_cast("STOCK_ON_HAND" as integer) as stock_on_hand,
+        current_timestamp() as ingestion_timestamp
+    from {{ source('public', 'inventory') }}
+ 
